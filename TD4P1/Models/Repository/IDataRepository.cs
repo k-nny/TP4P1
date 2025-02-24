c7f524a4d66b52448d7dbef0aa04699d23d25957
dodeyk <dodeyk@A275-S18>
2025-02-24 17:03:23 +0100

    fin tp4p2

---
+++ TD4P1/Models/Repository/IDataRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TD4P1.Models.Repository
+{
+    public interface IDataRepository<TEntity>
+    {
+        ActionResult<IEnumerable<TEntity>> GetAll();
+        ActionResult<TEntity> GetById(int id);
+        Task<ActionResult<TEntity>> GetByStringAsync(string str);
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entityToUpdate, TEntity entity);
+        Task DeleteAsync(TEntity entity);
+    }
+}
